d11fcb41396610d78aa3621b421f4f3ba4466713
skylakefreak <utkarshbarde2@gmail.com>
2024-09-06 01:31:49 +0530

    final

--- components/Main.jsx
+++ components/Main.jsx
@@ -2,8 +2,14 @@
 import React, { useState,useRef } from 'react'
 
 function Main() {
+    const [render,setrender]=useState(null)
     var adja=useRef([]);
+    var curr=useRef(true);
     const [blink,setblink]=useState(false);
+    const change=()=>{
+        curr.current=!curr.current
+        setrender(curr.current)
+    }
 
 
 
@@ -26,13 +32,13 @@ function Main() {
         if (Math.floor((box-20-1)/20>=0)){
             ans.push(box-20)
         }
-        adja=ans
+        adja.current=ans
         
         console.log(ans,"o")
 
 
     }
-    const [render,setrender]=useState(null)
+    
 
 
 
@@ -67,7 +73,17 @@ function Main() {
         var neighbours=[]
         const stack=[startbox];
         while(stack.length>0){
-            const current=stack.shift();
+            let current;
+            if (curr.current===false){
+                current=stack.shift();
+
+            }
+            else{
+                current=stack.pop();
+
+            }
+            
+            
             if (current==endbox){
                 setblink(true);
                 return
@@ -80,8 +96,7 @@ function Main() {
                 final.push(current);
                 visited.push(current);
                 adjacency(current)
-                console.log(adja)
-                neighbours=adja;
+                neighbours=adja.current;
                 for(let neighbour of neighbours){
                     stack.push(neighbour)
                 }
@@ -100,7 +115,7 @@ function Main() {
     }
   return (
     <div className='h-screen flex flex-col justify-center bg-gray-400 items-center'>
-        <div className='h-[800px] w-[800px] grid grid-cols-20 gap-1 p-1 grid-rows-20 bg-black'>
+        <div className='h-[40vh] w-[40vh] sm:h-[50vh] sm:w-[50vh] md:h-[80vh] md:w-[80vh] grid grid-cols-20  grid-rows-20 bg-white'>
             {boxes.map((box)=>(
                 
                 <div onClick={()=>{
@@ -109,10 +124,11 @@ function Main() {
                    
                     
                     
-                }} key={box} className={`bg-white transition-all duration-100 hover:bg-green-500  ${finalans.current.includes(box) ? 'bg-green-700' : ''}  ${box === startbox.current ? 'bg-green-700' : ''} ${box === endbox.current && blink==false ? 'bg-red-800' : ''} ${blink == true && box===endbox.current ? 'bg-green-700 animate-pulse' : ''}`}>
+                }} key={box} className={`transition-all duration-100 hover:bg-green-500 p-1 outline outline-1   ${finalans.current.includes(box) ? 'bg-green-700' : ''}  ${box === startbox.current ? 'bg-green-400' : ''} ${box === endbox.current && blink==false ? 'bg-red-800' : ''} ${blink == true && box===endbox.current ? 'bg-green-700 animate-pulse' : ''}`}>
 
                 </div>
             ))}
+        
            
 
         </div>
@@ -121,7 +137,19 @@ function Main() {
                 
 
             }} className='text-white p-2 pl-6 pr-6 m-2 rounded-sm hover:bg-blue-900 transition-all duration-100 bg-blue-700'>Find</button>
-            <button typeof='toggle'></button>
+           
+            <div onClick={()=>{
+                change()
+            }} className='flex  p-2 pl-6 pr-6 m-2 flex-row'>
+                <div className='flex flex-row items-center justify-center'>
+                <a className={`p-1 ${curr.current === true ? 'bg-black text-white' : 'bg-white text-black'} hover:cursor-pointer`}>DFS</a>
+                <a className={`p-1 hover:cursor-pointer ${curr.current===false ? 'bg-black text-white ': 'bg-white text-black'} `}>BFS</a>
+                </div>
+                <p className='flex items-center justify-center p-2'>Toggle Me</p>
+                
+                
+            </div>
+            
 
     </div>
   )
